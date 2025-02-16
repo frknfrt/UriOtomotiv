e0692917290b3514ec7b72159c0cd578caaadae6
yavuzselimfrat <yavuzselimfirat@icloud.com>
2025-02-16 23:37:44 +0300

    ysf commit

--- src/components/productList.js
+++ src/components/productList.js
@@ -1,32 +1,28 @@
-  const products = [
-    {
-      id: 1,
-      name: 'Product 1',
-      description: 'This is a description of Product 1.',
-      price: '$20',
-      image: 'https://via.placeholder.com/200', // Ürün resmi
-    },
-    {
-      id: 2,
-      name: 'Product 2',
-      description: 'This is a description of Product 2.',
-      price: '$30',
-      image: 'https://via.placeholder.com/200',
-    },
-    {
-      id: 3,
-      name: 'Product 3',
-      description: 'This is a description of Product 3.',
-      price: '$40',
-      image: 'https://via.placeholder.com/200',
-    },
-    {
-      id: 4,
-      name: 'Product 4',
-      description: 'This is a description of Product 4.',
-      price: '$50',
-      image: 'https://via.placeholder.com/200',
-    },
-  ];
+const products = [
+  {
+    id: 1,
+    name:'Solideal Havalı Forklift Lastik',
+    description: 'Solideal havalı lastikler, sağlam yapısı ve yüksek konforuyla iş makineleri ve ağır araçlar için ideal olup, uzun ömürlü kullanım sunar.',
+    image: '/images/lastik2.jpg', // Ürün resmi
+  },
+  {
+    id: 2,
+    name: 'Solideal Havalı Forklift Lastik',
+    description: 'Solideal havalı lastikler, dayanıklılığı ve konforu bir arada sunan, özellikle iş makineleri ve ağır hizmet araçları için tasarlanmış lastiklerdir.',
+    image: '/images/uriotomotivlastik3.png',
+  },
+  {
+    id: 3,
+    name: 'Solideal Havalı Forklift Lastik',
+    description: 'Solideal havalı lastikler, ağır hizmet araçları ve iş makineleri için geliştirilmiş, dayanıklı ve konforlu lastiklerdir. Zorlu arazilerde üstün performans sunar.',
+    image: '/images/uriotomotivlastik2.png',
+  },
+  {
+    id: 4,
+    name: 'Solideal Havalı Forklift Lastik',
+    description: 'Solideal havalı lastikler, zorlu koşullara dayanıklı, iş makineleri ve ağır hizmet araçları için üretilmiş, güvenilir ve konforlu lastiklerdir.',
+    image: '/images/uriotomotivlastik4.png',
+  },
+];
 
-  export { products };
+export { products };
