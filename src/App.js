3705ffcad3ecceebc921a5730e9c5ff3a79cc4e2
yavuzselimfrat <yavuzselimfirat@icloud.com>
2025-02-24 23:49:43 +0300

    telefon wp ikonları

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import './AppStyle.css'
-import { BrowserRouter,Route, Routes } from 'react-router-dom';
+import './AppStyle.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Products from './components/products';
-import './AppStyle.css';
+import FloatingIcons from './components/common/FloatingIcons'; // İkonları import ettik!
 
-
-const App = () =>  {
+const App = () => {
   return (
     <div>
       <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/about' element={<About/>}/>
-        <Route path='/products/:width' element={<Products/>}/>
-      </Routes>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/products/:width' element={<Products />} />
+        </Routes>
+        
+        {/* FloatingIcons bileşenini Router dışında ama Routes içinde çağırıyoruz */}
+        <FloatingIcons />
       </BrowserRouter>
     </div>
   );
--- src/components/NavMenuItems.js
+++ src/components/NavMenuItems.js
@@ -1,6 +1,6 @@
 
 const LastiEbatMenuLabels=["18x7-8", "21x8-9","23x9-10","23x10-12","27x10-12", "28x9-15","250-15","300-15","400-8","500-8","600-9","650-10","700-12", "700-15","750-10","750-15","750-16","825-25","900-20","1000-20","1100-20","1200-20"]
-const markaMenuLabels=["GRECKSTER","SOLİDEAL"]
+const markaMenuLabels=["SOLİDEAL"]
 const lastikcesitMenuLabels=["Havalı","Dolgulu","Çemberli","İz Bırakmayan Lastik"]
 const NavMenuItems = [{
   key: 'sub1',
